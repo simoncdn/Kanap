7f06730c86c477f12ed26183a84896392ed8a250
Cardona Simon <simon.cdn@icloud.com>
2022-09-28 13:18:09 +0200

    "add product.js"

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,45 @@
 let queryString = window.location.search;
-console.log(queryString.slice(4));
 let id = queryString.slice(4);
 
+
+let colors_place = document.getElementById("colors");
+let sel_colors = document.getElementsByTagName("option");
+
+colors_place.remove(2);
+colors_place.remove(1);
+
 fetch(`http://localhost:3000/api/products/${id}`)
-    .then(res => res.json)
-    .then(data => console.log(data));
+    .then(res => {
+        if (res.ok) {
+            return res.json()
+        }
+    })
+    .then(data => {
+        
+        document.querySelector("article div.item__img > img")
+        .setAttribute('src', `${data.imageUrl}`);
+        document.querySelector("article div.item__img > img")
+        .setAttribute('alt', `${data.altTxt}`);
+        
+        document.getElementById("title")
+        .innerHTML = (`${data.name}`);
+        
+        document.getElementById("price")
+        .innerHTML = (`${data.price}`);
+        
+        document.getElementById("description")
+        .innerHTML = (`${data.description}`);
+
+        for (let i = 0; i < data.colors.length; i++) {
+
+            let element = data.colors[i];
+
+            let color = document.createElement('option');
+            color.setAttribute("value", `${element}`);
+            color.innerHTML = `${element}`;
+            colors_place.appendChild(color);
+        }
+    })
+    .catch(err => {
+        console.log(`une erreur est survenue ${err}`);
+    })
